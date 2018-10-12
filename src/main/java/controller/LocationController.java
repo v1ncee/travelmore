ea3d0a7aa7b45ba5ee223751293873e969258786
v1ncee <duchateau.vince@gmail.com>
2018-10-12 08:57:02 +0200

    Added additional files

---
+++ src/main/java/controller/LocationController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import be.thomasmore.travelmore.domain.Location;
+import be.thomasmore.travelmore.service.LocationService;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+public class LocationController {
+
+    private Location newLocation = new Location();
+
+    @Inject
+    private LocationService locationService;
+
+    public Location getNewLocation() {
+        return newLocation;
+    }
+
+    public void setNewLocation(Location newLocation) {
+        this.newLocation = newLocation;
+    }
+
+    public List<Location> getLocations(){
+        return this.locationService.findAllLocations();
+    }
+
+    public void submit(){
+        this.locationService.insert(newLocation);
+    }
+
+}
