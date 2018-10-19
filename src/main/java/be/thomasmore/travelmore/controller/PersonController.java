0dceaedcbc30ad0beab575f866882da7a94422b4
r0705232 <r0705232@student.thomasmore.be>
2018-10-19 09:29:39 +0200

    fix index

--- src/main/java/be/thomasmore/travelmore/controller/PersonController.java
+++ src/main/java/be/thomasmore/travelmore/controller/PersonController.java
@@ -1,4 +1,36 @@
 package be.thomasmore.travelmore.controller;
 
+import be.thomasmore.travelmore.domain.Person;
+import be.thomasmore.travelmore.service.PersonService;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
 public class PersonController {
+
+    private Person newPerson = new Person();
+
+    @Inject
+    private PersonService personService;
+
+    public Person getNewPerson() {
+        return newPerson;
+    }
+
+    public void setNewPerson(Person newPerson) {
+        this.newPerson = newPerson;
+    }
+
+    public List<Person> getPersons(){
+        return this.personService.findAllPersons();
+    }
+
+    public void submit(){
+        this.personService.insert(newPerson);
+    }
+
 }
--- src/main/java/be/thomasmore/travelmore/domain/Person.java
+++ src/main/java/be/thomasmore/travelmore/domain/Person.java
@@ -6,8 +6,22 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "person")
-
+@NamedQueries(
+        {
+                @NamedQuery(
+                        name = Person.FIND_BY_ID,
+                        query = "SELECT p FROM Person p WHERE p.id = :id"
+                ),
+                @NamedQuery(
+                        name = Person.FIND_ALL,
+                        query = "SELECT p FROM Person p"
+                )
+        }
+)
 public class Person {
+    public static final String FIND_ALL = "Person.findAll";
+    public static final String FIND_BY_ID = "Person.findByID";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "PersonID")
@@ -18,6 +32,58 @@ public class Person {
     private String lastName;
     @Column(name = "Email")
     private String email;
+    @Column(name = "PassKey")
+    private String passKey;
+    @Column(name = "TypeUser")
+    private String typeUser;
 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassKey() {
+        return passKey;
+    }
+
+    public void setPassKey(String passKey) {
+        this.passKey = passKey;
+    }
+
+    public String getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(String typeUser) {
+        this.typeUser = typeUser;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
