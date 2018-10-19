618ff719d7daae7c605a420acb5b128a9c7c44e2
r0705232 <r0705232@student.thomasmore.be>
2018-10-19 11:05:23 +0200

    booking

--- src/main/java/be/thomasmore/travelmore/repository/BookingRepository.java
+++ src/main/java/be/thomasmore/travelmore/repository/BookingRepository.java
@@ -1,4 +1,27 @@
 package be.thomasmore.travelmore.repository;
 
+import be.thomasmore.travelmore.domain.Booking;
+import be.thomasmore.travelmore.domain.Location;
+import be.thomasmore.travelmore.domain.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 public class BookingRepository {
+    @PersistenceContext(unitName = "travelMorePU")
+    private EntityManager entityManager;
+
+    public Booking findById(int id) {
+        return entityManager.find(Booking.class, id);
+    }
+
+    public List<Booking> findAll() {
+        return entityManager.createNamedQuery(Booking.FIND_ALL, Booking.class).getResultList();
+    }
+
+    public void insert(Booking booking) {
+        entityManager.persist(booking);
+    }
+
 }
