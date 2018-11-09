a5ee4e1d2b972d126f2e58a9f5ff1af2005420d2
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-09 10:24:00 +0100

    merge fixed

--- src/main/java/be/thomasmore/travelmore/repository/TripRepository.java
+++ src/main/java/be/thomasmore/travelmore/repository/TripRepository.java
@@ -18,15 +18,12 @@ public class TripRepository {
         return entityManager.createNamedQuery(Trip.FIND_ALL, Trip.class).getResultList();
     }
 
-<<<<<<< HEAD
-=======
     public List<Trip> findByLocationName(String name) {
         if(name != "" && name != null){
             return entityManager.createNamedQuery(Trip.FIND_ALL_LOCATION_NAME, Trip.class).setParameter("name", name + "%").getResultList();
         }
         return null;
     }
->>>>>>> 6f2c1b8ff291c3618ad4a2c123105be7cec3fde1
 
     public void insert(Trip trip) {
         entityManager.persist(trip);
