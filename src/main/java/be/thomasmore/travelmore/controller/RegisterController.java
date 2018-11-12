d662a80eada324f972b8a664ce83e8ab2cead253
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-12 13:17:50 +0100

    mail works

--- src/main/java/be/thomasmore/travelmore/controller/RegisterController.java
+++ src/main/java/be/thomasmore/travelmore/controller/RegisterController.java
@@ -2,6 +2,7 @@ package be.thomasmore.travelmore.controller;
 
 import be.thomasmore.travelmore.service.PersonService;
 import be.thomasmore.travelmore.domain.Person;
+//import be.thomasmore.travelmore.controller.MailController;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -15,11 +16,16 @@ public class RegisterController {
 
     @Inject
     private PersonService personService;
+    private MailController mailController = new MailController();
 
     private String message;
 
     //register new user
     public String register(String firstName, String lastName, String email, String passKey) {
+        MailController mail = this.mailController;
+        String message;
+        String returnPage;
+
         //check if all fields are filled in
         if (!firstName.equals("") && !lastName.equals("") && !email.equals("") && !passKey.equals("")) {
 
@@ -33,22 +39,32 @@ public class RegisterController {
 
                 this.personService.insert(newPerson);
 
-                //redirect to login page
-                this.message = "Hi " + firstName + ", you are now registered. Please log in to book a trip.";
+                mail.send(email, firstName, lastName);
+                message = "Hi " + firstName + ", you are now registered. Please log in to book a trip.";
+                returnPage = "login";
             }
             else {
-                this.message = "This email is already in use. Please use another one.";
+                message = "This email is already in use. Please use another one.";
+                returnPage = "register";
             }
         }
         else {
-            this.message = "Make sure all fields are filled in correctly!";
+            message = "Make sure all fields are filled in correctly!";
+            returnPage = "register";
         }
 
-        return "message";
+        setMessage(message);
+
+        return returnPage;
     }
 
     //get message
     public String getMessage() {
         return this.message;
+    }
+
+    //set message
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
