3356e2d2fbb067b7a49f729f7ee0313739b0f8fc
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-12 17:02:40 +0100

    register works

--- src/main/java/be/thomasmore/travelmore/controller/RegisterController.java
+++ src/main/java/be/thomasmore/travelmore/controller/RegisterController.java
@@ -2,33 +2,31 @@ package be.thomasmore.travelmore.controller;
 
 import be.thomasmore.travelmore.service.PersonService;
 import be.thomasmore.travelmore.domain.Person;
-//import be.thomasmore.travelmore.controller.MailController;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class RegisterController {
 
     private Person newPerson = new Person();
+    private String message = "";
 
     @Inject
     private PersonService personService;
     private MailController mailController = new MailController();
 
-    private String message;
-
     //register new user
     public String register(String firstName, String lastName, String email, String passKey) {
         MailController mail = this.mailController;
-        String message;
+        String message1;
         String returnPage;
 
         //check if all fields are filled in
         if (!firstName.equals("") && !lastName.equals("") && !email.equals("") && !passKey.equals("")) {
-
             //check if email is already registered
             if (this.personService.findPersonByEmail(email) == null) {
                 newPerson.setFirstName(firstName);
@@ -40,27 +38,26 @@ public class RegisterController {
                 this.personService.insert(newPerson);
 
                 mail.send(email, firstName, lastName);
-                message = "Hi " + firstName + ", you are now registered. Please log in to book a trip.";
+                message1 = "Hi " + firstName + ", you are now registered. Please log in to book a trip.";
                 returnPage = "login";
-            }
-            else {
-                message = "This email is already in use. Please use another one.";
+            } else {
+                message1 = "This email is already in use. Please use another one.";
                 returnPage = "register";
             }
-        }
-        else {
-            message = "Make sure all fields are filled in correctly!";
+        } else {
+            message1 = "Make sure all fields are filled in correctly!";
             returnPage = "register";
         }
 
-        setMessage(message);
+
+        setMessage(message1);
 
         return returnPage;
     }
 
     //get message
     public String getMessage() {
-        return this.message;
+        return message;
     }
 
     //set message
